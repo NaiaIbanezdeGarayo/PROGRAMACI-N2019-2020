091f4271d64acb5997e93ec4b635281cbee7db4b
Naia <naia.idgalo@gmail.com>
2019-10-30 14:01:00 +0100

    EJERCICIO 9 ACABADO

--- TEMA 3/ej9/src/ej9/Ej9.java
+++ TEMA 3/ej9/src/ej9/Ej9.java
@@ -17,45 +17,49 @@ public class Ej9 {
     public static int cantUnidades = 0;
     public static int precUnitario = 0;
     public static String porIva;
-    public static int ivaNormal = 21;
-    public static int ivaReducido = 10;
-    public static int ivaSupereducido = 4;
-    public static int continuar;
-    public static String lineaDetalle;
-    public static int MULTIPLICACION = cantUnidades * precUnitario;
-    public static int almacen4;
-    public static int almacen10;
-    public static int almacen21;
-    public static double CALCULO4 = almacen4 * 0.04;
-    public static double CALCULO10 = almacen10 * 0.10;
-    public static double CALCULO21 = almacen21 * 0.21;
-    public static String TOTALIVA4 = "4% de " + almacen4 + " = " + CALCULO4;;
-    public static String TOTALIVA10 = "10% de " + almacen10 + " = " + CALCULO10;;
-    public static String TOTALIVA21 = "21% de " + almacen21 + " = " + CALCULO21;;
-    public static double totalesIva;
-    public static double TOTAL = totalesIva + almacen4 + almacen10 + almacen21; 
+    public static int continuar = 0;
+    public static String lineaDetalle = " ";
+    public static int multiplicacion = 0; 
+    public static int almacen4 = 0;
+    public static int almacen10 = 0;
+    public static int almacen21 = 0;
+    public static double calculo4;
+    public static double calculo10; 
+    public static double calculo21 ; 
+    public static String totalIva4;
+    public static String totalIva10 ;
+    public static String totalIva21 ;
+    public static double totalesIva = 0;
+    public static double total;  
     
     
     public static void main(String[] args) {
         // TODO code application logic here
         nombreCli = JOptionPane.showInputDialog("Introduce el nombre del cliente");
         do {
-            
-            cantUnidades = Integer.parseInt(JOptionPane.showInputDialog("Introduce la cantidad de unidades"));
-            precUnitario = Integer.parseInt(JOptionPane.showInputDialog("Introduce el precio unitario"));
-            porIva = JOptionPane.showInputDialog("Introduce el porcentaje de IVA (4% o 10% o 21%)");
-            if ("4".equals(porIva)) {
-                iva4();
+            try{
+                cantUnidades = Integer.parseInt(JOptionPane.showInputDialog("Introduce la cantidad de unidades"));
+                precUnitario = Integer.parseInt(JOptionPane.showInputDialog("Introduce el precio unitario"));
+                porIva = JOptionPane.showInputDialog("Introduce el porcentaje de IVA (4% o 10% o 21%)");
+                if ("4".equals(porIva)) {
+                    iva4();
+                }
+                if ("10".equals(porIva)) {
+                   iva10(); 
+                }
+                if ("21".equals(porIva)) {
+                    iva21();
+                }
+            }catch(NullPointerException e){
+                JOptionPane.showMessageDialog(null, "Escribe algo");
             }
-            if ("10".equals(porIva)) {
-               iva10(); 
+            catch(NumberFormatException e){
+                JOptionPane.showMessageDialog(null, "Tienes que escribir números");
             }
-            if ("21".equals(porIva)) {
-                iva21();
-            }
+            lineaDetalles();
             continuar = JOptionPane.showConfirmDialog(null, "¿Quieres introducir productos?");
         } while (continuar != 1);
-        lineaDetalle();
+        
         
         totalIva();
         
@@ -67,37 +71,45 @@ public class Ej9 {
     
         JOptionPane.showMessageDialog(null, "NOMBRE: " + nombreCli + "\n" +
                                       "LINEAS DE DETALLE: " + "\n" + lineaDetalle + "\n" + 
-                                      "TOTAL SIN IVA: " + TOTALIVA4 + "\n" + TOTALIVA10 + "\n" + TOTALIVA21 + "\n" +
+                                      "TOTAL SIN IVA: " + "\n" + totalIva4 + "\n" + totalIva10 + "\n" + totalIva21 + "\n" +
                                       "TOTAL IVA: " + totalesIva + "\n" +
-                                      "TOTAL CON IVA: "  );
+                                      "TOTAL CON IVA: " + total  );
         
 
     }
 
-    private static void lineaDetalle() {
+    private static void lineaDetalles() {
         
         
-        lineaDetalle += cantUnidades + " X " + precUnitario + " = " + MULTIPLICACION; 
+        lineaDetalle += cantUnidades + " X " + precUnitario + " = " + (cantUnidades * precUnitario) + "\n"; 
     }
 
     
 
     private static void totalIva() {
-        totalesIva = CALCULO4 + CALCULO10 + CALCULO21;
+        calculo4 = almacen4 * 0.04;
+        calculo10 = almacen10 * 0.10;
+        calculo21 = almacen21 * 0.21;
+        totalesIva = calculo4 + calculo10 + calculo21;
+        
+        totalIva4 =  "4% de " + almacen4 + " = " + calculo4;
+        totalIva10 = "10% de " + almacen10 + " = " + calculo10;
+        totalIva21 = "21% de " + almacen21 + " = " + calculo21;
+        total = totalesIva + almacen4 + almacen10 + almacen21;
     }
 
     
 
     private static void iva4() {
-        almacen4 += MULTIPLICACION;
+        almacen4 += cantUnidades * precUnitario;
     }
 
     private static void iva10() {
-        almacen10 += MULTIPLICACION;
+        almacen10 += cantUnidades * precUnitario;
     }
 
     private static void iva21() {
-        almacen21 += MULTIPLICACION;
+        almacen21 += cantUnidades * precUnitario;
     }
     
 }
