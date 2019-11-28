3e1132bbe5d50edc20c1bb55aea655896a943ad3
Naia <naia.idgalo@gmail.com>
2019-11-28 12:25:42 +0100

    EJERCICIO 11 TERMINADO

--- TEMA 4/ej11/src/ej11/Ej11.java
+++ TEMA 4/ej11/src/ej11/Ej11.java
@@ -17,9 +17,13 @@ public class Ej11 {
     /**
      * @param args the command line arguments
      */
+    public static String [][] golosina;
+    public static double [][] precio;
+    public static int [][] cantidad;
+    public static int [][] ventasC;
     public static void main(String[] args) {
         // TODO code application logic here
-        String [][] golosina = new String [4][4];
+        golosina = new String [4][4];
         golosina [0][0] = "KitKat";
         golosina [0][1] = "Chicles Fresa";
         golosina [0][2] = "Lacasitos";
@@ -37,7 +41,7 @@ public class Ej11 {
         golosina [3][2] = "Milkybar";
         golosina [3][3] = "KitKat";
         
-        double [][] precio = new double [4][4];
+        precio = new double [4][4];
         precio [0][0] = 1.1;
         precio [0][1] = 0.8;
         precio [0][2] = 1.5;
@@ -55,35 +59,26 @@ public class Ej11 {
         precio [3][2] = 1.1;
         precio [3][3] = 1.1;
         
-        int [][] cantidad = new int [4][4];
+        cantidad = new int [4][4];
         for (int i = 0; i < cantidad.length; i++) {
             for (int j = 0; j < cantidad.length; j++) {
                 cantidad[i][j]=5;
             }
         }
-        /*cantidad [0][0] = 5;
-        cantidad [0][1] = 5;
-        cantidad [0][2] = 5;
-        cantidad [0][3] = 5;
-        cantidad [1][0] = 5;
-        cantidad [1][1] = 5;
-        cantidad [1][2] = 5;
-        cantidad [1][3] = 5;
-        cantidad [2][0] = 5;
-        cantidad [2][1] = 5;
-        cantidad [2][2] = 5;
-        cantidad [2][3] = 5;
-        cantidad [3][0] = 5;
-        cantidad [3][1] = 5;
-        cantidad [3][2] = 5;
-        cantidad [3][3] = 5;*/
-        /*Arrays.fill(cantidad,5);*/
+        
+        ventasC = new int [4][4];
+        for (int i = 0; i < ventasC.length; i++) {
+            for (int j = 0; j < ventasC.length; j++) {
+                ventasC[i][j]=0;
+            }
+        }
+        
         
         menu();
     }
 
     private static void menu() {
-        int m = Integer.parseInt(JOptionPane.showInputDialog("Introduce una opción"
+        int m = Integer.parseInt(JOptionPane.showInputDialog("Introduce una opción\n"
                 + "1. Pedir golosina \n"
                 + "2. Mostrar golosinas \n"
                 + "3. Rellenar golosinas \n"
@@ -102,19 +97,76 @@ public class Ej11 {
     }
 
     private static void pedirGolosina() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int continuar ;
+        do {
+            String fc = JOptionPane.showInputDialog("Introduce el número del producto");
+            int p1 = Integer.parseInt(Character.toString(fc.charAt(0)));
+            int p2 = Integer.parseInt(Character.toString(fc.charAt(1)));
+            if ((p1 >= 0 && p1 <=3) && (p2 >= 0 && p2 <=3)) {
+                if (cantidad[p1][p2] == 0) {
+                    JOptionPane.showMessageDialog(null, "El producto seleccionado está agotado");
+                }else
+                    cantidad[p1][p2] = cantidad[p1][p2] -1;
+                    ventasC[p1][p2] = ventasC[p1][p2] +1;
+
+            }else
+                JOptionPane.showMessageDialog(null, "El número introducido es erróneo");
+            continuar = JOptionPane.showConfirmDialog(null, "Puede recoger el producto: " + golosina[p1][p2] + " con precio:" + precio[p1][p2] + "\n" 
+                                                        + "¿Quiere sacar más productos?");
+            
+        } while (continuar != 1);
+        
+        menu();
     }
 
     private static void mostrarGolosinas() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String texto = "";
+        for (int i = 0; i < golosina.length; i++) {
+            for (int j = 0; j < golosina.length; j++) {
+                if (cantidad[i][j] != 0) {
+                    texto += "Código del producto: " + i + ""+ j + "" + " Nombre del producto: " + golosina[i][j] + " Precio: " + precio[i][j] + "\n";
+                }
+                
+            }
+        }
+        JOptionPane.showMessageDialog(null, texto);
+        menu();
     }
 
     private static void rellenarGolosinas() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int continuar = 3 ;
+        
+        
+            String con = JOptionPane.showInputDialog("Introduce la contraseña");
+            if (con.equals("MaquinaExpendedora2019")) {
+                do {
+                    String cod = JOptionPane.showInputDialog("Introduce primero el código del producto");
+                    int p1 = Integer.parseInt(Character.toString(cod.charAt(0)));
+                    int p2 = Integer.parseInt(Character.toString(cod.charAt(1)));
+                    if ((p1 >= 0 && p1 <=3) && (p2 >= 0 && p2 <=3)) {
+                        int cant = Integer.parseInt(JOptionPane.showInputDialog("Introduce la cantidad que vas a meter"));
+                        cantidad[p1][p2] += cant;
+                        JOptionPane.showMessageDialog(null, "Se ha introducido al producto " + cod + ": " + golosina[p1][p2] +  " esta cantidad: " + cant );
+                        continuar = JOptionPane.showConfirmDialog(null, "¿Quieres introducir más productos?");
+                        }
+                } while (continuar != 1);
+                menu();
+            }else
+                JOptionPane.showMessageDialog(null, "La contraseña introducida es errónea");
+                menu();
+        
     }
 
     private static void apagarMaquina() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String texto ="";
+        for (int i = 0; i < golosina.length; i++) {
+            for (int j = 0; j < golosina.length; j++) {
+                texto += "Código del producto: " + i + ""+ j + "" + " Nombre del producto: " + golosina[i][j] + " Cantidades vendidas: " + ventasC[i][j] + "\n"; 
+            }
+        }
+        JOptionPane.showMessageDialog(null, texto);
+        System.exit(0);
+        
     }
     
 }
