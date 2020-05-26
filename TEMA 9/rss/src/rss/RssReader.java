afd737481c41d917e71e3e52cfb4d082de0161af
Naia <naia.idgalo@gmail.com>
2020-05-26 22:27:56 +0200

    rss

--- TEMA 9/rss/src/rss/RssReader.java
+++ TEMA 9/rss/src/rss/RssReader.java
@@ -4,18 +4,50 @@
  * and open the template in the editor.
  */
 package rss;
-
+import Vista.VistaFeed;
+import java.io.*;
+import java.net.*;
+import javax.swing.JOptionPane;
 /**
  *
  * @author Naia
  */
-public class Rss {
+public class RssReader {
 
-    /**
-     * @param args the command line arguments
-     */
+    private static VistaFeed vp;
     public static void main(String[] args) {
-        // TODO code application logic here
+        vp = new VistaFeed();
+        vp.setLocationRelativeTo(null);
+        vp.setVisible(true);
+    }
+    public static String readRSS(String DirUrl){
+        try {
+            URL rssUrl = new URL(DirUrl);
+            BufferedReader in = new BufferedReader(new InputStreamReader(rssUrl.openStream()));
+            String sourceCode = "";
+            String line;
+            while ((line = in.readLine())!= null) {                
+                if (line.contains("<tittle>")) {
+                    int firstPos = line.indexOf("<tittle>");
+                    String temp = line.substring(firstPos);
+                    temp = temp.replace("<tittle>", "");
+                    int lastPos = temp.indexOf("<tittle>");
+                    temp = temp.substring(0, lastPos);
+                    sourceCode += temp + "\n";
+                }
+ 
+            }
+        
+            in.close();
+            return sourceCode;
+        } 
+        catch (MalformedURLException ue) {
+            System.out.println("Malformed URL");
+        }
+        catch (IOException ioe){
+            System.out.println("Something went wrong reading then contents");
+        }
+        return null;
     }
     
 }
